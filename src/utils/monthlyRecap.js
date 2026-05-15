0cccd0ab7cc3b9db9b91867398e4224827f4ca39
spenttlive <spentt.live@gmail.com>
2026-05-15 19:38:32 +0530

    test monthly view

--- src/utils/monthlyRecap.js
+++ src/utils/monthlyRecap.js
@@ -3,12 +3,9 @@ import { generateVerdict } from './receipt'
 
 export function getLastMonthExpenses(expenses) {
   const now = new Date()
-  const lastMonth = now.getMonth() === 0 ? 11 : now.getMonth() - 1
-  const lastMonthYear = now.getMonth() === 0 ? now.getFullYear() - 1 : now.getFullYear()
-
   return expenses.filter((e) => {
     const d = new Date(e.ts)
-    return d.getMonth() === lastMonth && d.getFullYear() === lastMonthYear
+    return d.getMonth() === now.getMonth() && d.getFullYear() === now.getFullYear()
   })
 }
 
