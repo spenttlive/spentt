070860e73c136c337ede6f808333928e0ac36cff
spenttlive <spentt.live@gmail.com>
2026-06-23 18:38:58 +0530

    Add TinyLaunch badge to footer with link

--- src/screens/LandingScreen.jsx
+++ src/screens/LandingScreen.jsx
@@ -436,8 +436,8 @@ export default function LandingScreen({ onGetStarted }) {
         <a href="https://www.foundrlist.com/product/spentt?utm_source=badge&utm_medium=embed" target="_blank" rel="noopener">
         <img src="https://www.foundrlist.com/api/badge/spentt" alt="Featured on FoundrList" width="150" height="48" />
         </a>
-        <a href="https://www.tinylaunch.com/launch/14692" target="_blank" rel="noopener">
-        <img src="https://tinylaunch.com/tinylaunch_badge_featured_on.svg" alt="TinyLaunch Badge" style={{ width: 202, height: 'auto' }} />
+        <a href="https://www.tinystartups.com/startup/spentt" target="_blank" rel="noopener noreferrer">
+        <img src="https://www.tinystartups.com/api/badge/featured/spentt" alt="Featured on Tiny Startups" />
         </a>
       </footer>
     </div>
